c3a3419ed5ee0fd7fcccbea47fa003302d6b1c7f
Caleb <caleb.cipra@rocketbot.com>
2022-07-01 10:11:25 -0400

    [hidden] first commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Clientify
+  
+Module to interact with Clientify. Get deals with filters.  
+
+## How to install this module
+  
+__Download__ and __install__ the content in 'modules' folder in Rocketbot path
+
+## How to use this module
+  
+To use this module, you have to connecto to your account with "Connects to Clientify" and then get deals with "Get deals with filters" and use the filters if you need them.
+
+## Overview
+
+
+1. Connects to Clientify  
+Connects to Clientify
+
+2. Get deals with filters  
+Gets all your deals with filters
+
+----
+### OS
+
+- windows
+
+### Dependencies
+
+- [**requests**](https://pypi.org/project/requests/)
+
+### License
+  
+![MIT](https://camo.githubusercontent.com/107590fac8cbd65071396bb4d04040f76cde5bde/687474703a2f2f696d672e736869656c64732e696f2f3a6c6963656e73652d6d69742d626c75652e7376673f7374796c653d666c61742d737175617265)  
+[MIT](http://opensource.org/licenses/mit-license.ph)
---
+++ __init__.py
@@ -0,0 +1,120 @@
+# coding: utf-8
+"""
+Base para desarrollo de modulos externos.
+Para obtener el modulo/Funcion que se esta llamando:
+     GetParams("module")
+
+Para obtener las variables enviadas desde formulario/comando Rocketbot:
+    var = GetParams(variable)
+    Las "variable" se define en forms del archivo package.json
+
+Para modificar la variable de Rocketbot:
+    SetVar(Variable_Rocketbot, "dato")
+
+Para obtener una variable de Rocketbot:
+    var = GetVar(Variable_Rocketbot)
+
+Para obtener la Opcion seleccionada:
+    opcion = GetParams("option")
+
+
+Para instalar librerias se debe ingresar por terminal a la carpeta "libs"
+    
+    pip install <package> -t .
+
+"""
+
+import os
+import sys
+
+base_path = tmp_global_obj["basepath"]
+cur_path = base_path + 'modules' + os.sep + 'Clientify' + os.sep + 'libs' + os.sep
+if cur_path not in sys.path:
+    sys.path.append(cur_path)
+
+from clientifyObject import ClientifyObject
+
+global clientify_I
+
+module = GetParams("module")
+
+try:
+
+    if (module == "connectToClientify"):
+
+        username = GetParams("username")
+        password = GetParams("password")
+        
+        clientify_I = ClientifyObject(username, password)
+
+        resultConnection = False
+
+        if clientify_I.token != "":
+            resultConnection = True
+        
+        whereToStore = GetParams("whereToStore")
+        SetVar(whereToStore, resultConnection)
+    
+    if (module == "getDealsWithQuery"):
+        
+        deals_filter = GetParams("deals_filter")
+        if deals_filter:
+            deals_filter = "query=" + deals_filter
+        else:
+            deals_filter = ""
+        
+        status_id = GetParams("status_id")
+        if status_id:
+            status_id = "status_id=" + status_id
+        else:
+            status_id = ""
+
+        date_filter = GetParams("date_filter")
+        date_filter_1 = ""
+        date_filter_2 = ""
+        
+        if date_filter:
+            greater_filter = GetParams("greater_filter")
+            if greater_filter:   
+                date_greater = GetParams("date_greater")
+                date_filter_1 += f"{date_filter}{greater_filter}={date_greater}"
+            lesser_filter = GetParams("lesser_filter")
+            if lesser_filter:
+                date_lesser = GetParams("date_lesser")
+                date_filter_2 += f"{date_filter}{lesser_filter}={date_lesser}"
+        
+        owner_name = GetParams("owner_name")
+        if not owner_name:
+            owner_name = ""
+
+        query = f"{deals_filter}&{status_id}&{date_filter_1}&{date_filter_2}"
+
+        closed_date_greater_filter = GetParams("closed_date_greater_filter")
+        closed_date_greater = GetParams("closed_date_greater")
+        closed_date_lesser_filter = GetParams("closed_date_lesser_filter")
+        closed_date_lesser = GetParams("closed_date_lesser")
+        
+        actual_closed_date = {"gt" : False, "gte" : False, "lt" : False, "lte" : False, "gt_date" : "", "gte_date" : "", "lt_date" : "", "lte_date" : ""}
+        
+        if closed_date_greater_filter:
+            closed_date_greater = closed_date_greater.replace("/", "-")
+            actual_closed_date[closed_date_greater_filter] = True
+            actual_closed_date[f"{closed_date_greater_filter}_date"] = closed_date_greater
+        if closed_date_lesser_filter:
+            closed_date_lesser = closed_date_lesser.replace("/", "-")
+            actual_closed_date[closed_date_lesser_filter] = True
+            actual_closed_date[f"{closed_date_lesser_filter}_date"] = closed_date_lesser
+
+        pipeline_desc = GetParams("pipeline_desc")
+        if not pipeline_desc:
+            pipeline_desc = ""
+
+        resultRead = clientify_I.getDealsWithQuery(query, owner_name, actual_closed_date, pipeline_desc)
+
+        whereToStore = GetParams("whereToStore")
+        SetVar(whereToStore, resultRead)
+
+except Exception as e:
+    print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
+    PrintException()
+    raise e
